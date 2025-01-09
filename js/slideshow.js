989a12c6bac4e5b963fff0b36f5c03795772b0d3
sarmenti7 <84134786+sarmenti7@users.noreply.github.com>
2025-01-09 10:42:55 -0500

    Update slideshow.js

--- js/slideshow.js
+++ js/slideshow.js
@@ -3,10 +3,10 @@ function showSlides() {
 
   /*
 	TO-DO: Get ALL slide elements.
-         Loop through all the slides and hide them (Hint: Use DOM display property).
-         Increment the slide index.
-         If the slide index exceeds the total number of slides, reset the slide index to 1. 
-         Display the current slide (Hint: Use DOM display property).
+               Loop through all the slides and hide them (Hint: Use DOM display property).
+               Increment the slide index.
+               If the slide index exceeds the total number of slides, reset the slide index to 1. 
+               Display the current slide (Hint: Use DOM display property).
 
   */
 
@@ -19,6 +19,6 @@ function showSlides() {
 
 /*
 	TO-DO: Create a variable to keep track of the slide index and initialize to 0.
-         Call the function above.     	
+               Call the function above.     	
 */
 
