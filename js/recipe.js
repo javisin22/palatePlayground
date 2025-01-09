df20d82b1d16a435a6ebdd27a215f82c5e67ccfd
sarmenti7 <84134786+sarmenti7@users.noreply.github.com>
2025-01-09 10:26:35 -0500

    Update recipe.js

--- js/recipe.js
+++ js/recipe.js
@@ -1,54 +1,36 @@
-/*
-	TO-DO: Create an object using constructor notation 
-		   Template should include parameters to set values of properties and getStars method:
-		   	 - Parameters (card number, image name, recipe name, recipe time)
-		     - getStars method gets # of stars to create and uses following code to create star rating: 
+// Array of image filenames for different recipes
+var images = ['burger.jpeg', 'frenchToast.jpeg', 'omelette.jpeg', 'pancake.jpeg', 'pasta.jpeg', 'salad.jpeg', 'sandwich.jpeg', 'steak.webp', 'tacos.jpeg'];
 
-					var star = '<span style="color:darkgoldenrod;">&#9733;</span>';
-					var rating = star.repeat(numStars);
-					return rating;		     	
+// Array of recipe names corresponding to the images
+var recipes = ['Beefy Burger', 'Gooey French Toast', 'Eggy Omelette', 'Cherry Pancakes', 'Saucy Pasta', 'Fancy Salad', 'Club Sandwich', 'Hearty Steak', 'Avocado Tacos'];
 
-*/
+// Array of preparation times (in minutes) for each recipe
+var times = [15, 10, 15, 20, 10, 20, 30, 50, 20];
 
-
-/*
-	Function to display a recipe card
+// Array of star ratings for each recipe
+var stars = [5, 3, 4, 3, 1, 4, 3, 4, 5];
 
 
-	TO-DO: Replace EACH ? with the appropriate parameter name in this function
+// Function to generate a string of star icons based on the number of stars
+function getStars(numStars) {
 
-*/
-function displayRecipeCard(?, ?, ?, ?, ?) {
-	// Get the card element using its cardNum
-	var card = document.getElementsByClassName('card')[?];
+    // HTML code for a single star icon with a dark goldenrod color
+    var star = '<span style="color:darkgoldenrod;">&#9733;</span>';
 
+    // Repeat the star icon to create the rating string
+    var rating = star.repeat(numStars);
 
-	card.innerHTML += '<img src="../palatePlayground/images/' + ? + '">';
-	card.innerHTML += '<h3>' + ? + '</h3><hr />';
-	card.innerHTML += '<p>' + ? + ' mins &emsp;&emsp;&emsp;' + ? + '</p>';
-};
-
-/*
-	TO-DO: Create the following instances of the recipe object:
-			- French Toast
-			- Omelette
-			- Pancake
-			- Pasta
-			- Salad
-			- Sandwich
-			- Burger
-			- Steak
-			- Taco
-
-			You can make up the names and times for each recipe BUT make sure to use the 
-			images given to you in the images folder!
-*/
+    // Return the generated rating string
+    return rating;
+}
 
 
 /*
-	TO-DO: Access each property and method for ALL the recipe objects you created
-		   and use them as arguments when calling the displayRecipeCard function.
-
-
+	TO-DO: Get ALL card elements.
+	       Loop through all the cards and update their content to include the following:
+           	- An image element with the src attribute set to the corresponding image file in the images array.
+           	- An h3 element with the recipe name from the recipes array.
+           	- A horizontal rule element.
+           	- A paragraph element with the preparation time and star rating (Hint: Use the getStars function).	     	
 */
 
